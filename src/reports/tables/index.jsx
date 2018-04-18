71836267ad988625201d6f3fd04c4562bc2cf077
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2018-04-18 14:26:20 -0700

    Adds table 12-2

--- src/reports/tables/index.jsx
+++ src/reports/tables/index.jsx
@@ -2,10 +2,14 @@ import One from './One.jsx'
 import Four from './Four.jsx'
 import Five from './Five.jsx'
 import Eleven from './Eleven.jsx'
+import TwelveOne from './TwelveOne.jsx'
+import TwelveTwo from './TwelveTwo.jsx'
 
 export default {
   One,
   Four,
   Five,
-  Eleven
+  Eleven,
+  TwelveOne,
+  TwelveTwo
 }
