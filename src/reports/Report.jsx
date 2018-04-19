748b8341b3cc4b6275e78f6af4a4b5c23d1e6989
Andrew <awolfe76@gmail.com>
2018-04-19 09:23:13 -0400

    adds report 2

--- src/reports/Report.jsx
+++ src/reports/Report.jsx
@@ -41,6 +41,7 @@ class Report extends React.Component {
 
   selectReport(report) {
     if (report.table.match(/^1$/)) return <Tables.One report={report} />
+    if (report.table.match(/^2$/)) return <Tables.Two report={report} />
     if (report.table.match(/^4-/)) return <Tables.Four report={report} />
     if (report.table.match(/^5-/)) return <Tables.Five report={report} />
     if (report.table.match(/^11-/)) return <Tables.Eleven report={report} />
--- src/reports/tables/index.jsx
+++ src/reports/tables/index.jsx
@@ -1,10 +1,12 @@
 import One from './One.jsx'
+import Two from './Two.jsx'
 import Four from './Four.jsx'
 import Five from './Five.jsx'
 import Eleven from './Eleven.jsx'
 
 export default {
   One,
+  Two,
   Four,
   Five,
   Eleven
