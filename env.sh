eeb30e61b41bf0a309b16d00ec4dbcc1d285b57d
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2018-05-21 18:36:42 -0700

    update envvar usage

--- env.sh
+++ env.sh
@@ -10,19 +10,19 @@ HMDA_API_SERVER=$(get_baseurl ${HMDA_API})
 
 if [ -f ./data-publication/js/app.min.js.bak ]; then
   sed \
-    -e "s@##APP_URL##@${APP_URL:-https://192.168.99.100}@"\
+    -e "s@##HOMEPAGE_URL##@${HOMEPAGE_URL:-https://192.168.99.100}@"\
     -e "s@##HMDA_API##@${HMDA_API:-https://192.168.99.100:4443/hmda}@"\
     ./data-publication/js/app.min.js.bak > ./data-publication/js/app.min.js
 else
   sed -i.bak \
-    -e "s@##APP_URL##@${APP_URL:-https://192.168.99.100}@"\
+    -e "s@##HOMEPAGE_URL##@${HOMEPAGE_URL:-https://192.168.99.100}@"\
     -e "s@##HMDA_API##@${HMDA_API:-https://192.168.99.100:4443/hmda}@"\
     ./data-publication/js/app.min.js
 fi
 
 if [ -f /etc/nginx/nginx.tmpl ]; then
   sed \
-  -e "s@##APP_SERVER##@${APP_URL:-https://192.168.99.100}@g"\
+  -e "s@##APP_SERVER##@${HOMEPAGE_URL:-https://192.168.99.100}@g"\
   -e "s@##HMDA_API_SERVER##@${HMDA_API_SERVER:-https://192.168.99.100:4443}@g"\
   /etc/nginx/nginx.tmpl > /etc/nginx/nginx.conf
 fi
