3ca5418629a3cc15637f081f03d5f9159bd68d85
Andrew <awolfe76@gmail.com>
2019-02-15 08:32:31 -0500

    fix spacing, centering

--- src/common/NotFound.jsx
+++ src/common/NotFound.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Header from './Header.jsx'
 
+import './NotFound.css'
+
 const NotFound = () => {
   return (
-    <div className="usa-grid" id="main-content">
-      <div className="usa-width-one-whole">
-        <Header
-          type={1}
-          headingText="Sorry, something went wrong."
-          paragraphText="We can't seem to find the page you are looking for."
-        >
-          <Link to="/">Return to the data publication home page.</Link>
-        </Header>
-      </div>
+    <div className="NotFound" id="main-content">
+      <Header
+        type={1}
+        headingText="Sorry, something went wrong."
+        paragraphText="We can't seem to find the page you are looking for."
+      >
+        <Link to="/">Return to the data publication home page.</Link>
+      </Header>
     </div>
   )
 }
