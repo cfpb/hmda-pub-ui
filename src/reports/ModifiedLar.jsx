265aec6fb36286d93815daa81e3bc2152f050921
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-03-21 19:42:10 -0700

    modified lar file spec links [deploy pr]

--- src/reports/ModifiedLar.jsx
+++ src/reports/ModifiedLar.jsx
@@ -14,22 +14,22 @@ const ModifiedLar = props => {
         <Header
           type={1}
           headingText="Modified Loan/Application Register (LAR)"
-          paragraphText="A downloadable modified LAR file is available for every 
-            financial institution that has completed a HMDA data submission in the selected year. 
-            Enter a financial institution’s name to download its modified LAR file. 
-            If you cannot find a particular financial institution using this form, the 
-            institution may not have been required to report HMDA data or it 
+          paragraphText="A downloadable modified LAR file is available for every
+            financial institution that has completed a HMDA data submission in the selected year.
+            Enter a financial institution’s name to download its modified LAR file.
+            If you cannot find a particular financial institution using this form, the
+            institution may not have been required to report HMDA data or it
             may not have completed its HMDA data submission."
         >
           <p>
-            The{' '}
+            Modified LAR file specifications: <a
+              title="Modified LAR 2018 file specification"
+              href="https://github.com/cfpb/hmda-platform/blob/master/docs/v2/spec/Modified%20LAR%20Schema.csv"
+            >2018</a>,{' '}
             <a
-              title="Modified LAR file specification"
+              title="Modified LAR 2017 file specification"
               href="https://github.com/cfpb/hmda-platform/blob/master/docs/v1/2017_Modified_LAR_Spec.csv"
-            >
-              modified LAR file specification
-            </a>{' '}
-            is now available.
+            >2017</a>
           </p>
         </Header>
         <YearSelector standalone={true} />
