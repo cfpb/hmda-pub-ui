a8c1d9c7cdb2de0cf60dcbbb5a3a4c5112b18326
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-04-02 11:18:27 -0700

    point to hmda4 [deploy pr]

--- src/reports/fetchMsas.js
+++ src/reports/fetchMsas.js
@@ -1,7 +1,7 @@
 function getMsaUrl(institutionId, year) {
   if(!year) return
   if(year === '2017') return `https://ffiec-api.cfpb.gov/public/filers/2017/${institutionId}/msaMds`
-  return `https://ffiec.cfpb.gov/v2/reporting/filers/${year}/${institutionId}/msaMds`
+  return `https://hmda4.demo.cfpb.gov/v2/reporting/filers/${year}/${institutionId}/msaMds`
 }
 
 export default function(institutionId, year) {
