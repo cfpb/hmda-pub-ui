a9b7d6b411ef3a1d9947f296781994c92de8d9b6
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-07-29 15:57:43 -0700

    fork s3 env based on year

--- src/reports/Report.jsx
+++ src/reports/Report.jsx
@@ -117,7 +117,7 @@ class Report extends React.Component {
     let year = params.year
     let msaMdId = params.msaMdId
     let reportId = params.reportId
-    const env = 'prod'
+    const env = year === '2017' ? 'prod' : 'dev'
     let ext = year === '2017' ? '.txt' : '.json'
     if(reportId === 'IRS') ext = '.csv'
     let url = `https://s3.amazonaws.com/cfpb-hmda-public/${env}/reports/`
