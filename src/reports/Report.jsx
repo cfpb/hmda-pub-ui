028cb4f1eb4c6a23d3fc9468e3fc0104c85558a1
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-08-22 13:18:08 -0700

    snapshot links and aggregation/disclosure updates

--- src/reports/Report.jsx
+++ src/reports/Report.jsx
@@ -116,7 +116,7 @@ class Report extends React.Component {
     let year = params.year
     let msaMdId = params.msaMdId
     let reportId = params.reportId
-    const env = year === '2018' && params.institutionId ? 'dev' : 'prod'
+    const env = 'prod'
     let ext = year === '2017' ? '.txt' : '.json'
     if(reportId === 'IRS') ext = '.csv'
     let url = `https://s3.amazonaws.com/cfpb-hmda-public/${env}/reports/`
