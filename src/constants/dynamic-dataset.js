68154c6bd3833a43871829d5d65f9f43429e90f0
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-08-26 08:41:21 -0700

    add 2018 dynamic data

--- src/constants/dynamic-dataset.js
+++ src/constants/dynamic-dataset.js
@@ -1,9 +1,9 @@
 export const DYNAMIC_DATASET = {
   2018: {
-    lar : '2018-lar',
-    ts : '2018-ts',
-    lar_spec : '2018-lar-specs',
-    ts_spec : '2018-ts-spec'
+    lar : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2018/2018_lar.txt',
+    ts : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2018/2018_ts.txt',
+    lar_spec : 'https://github.com/cfpb/hmda-platform/blob/master/docs/v2/spec/2018_File_Spec_LAR.csv',
+    ts_spec : 'https://github.com/cfpb/hmda-platform/blob/master/docs/v2/spec/2018_File_Spec_TS.csv'
   },
   2017: {
     lar: 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2017_lar.txt',
